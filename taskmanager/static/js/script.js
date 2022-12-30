31300530084fcf4428b7176a14c00b3e466d945e
Alan Cruz da Silva <alancruzsilva@gmail.com>
2022-12-30 16:44:15 +0000

    Add links js/css from materialize framework

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,7 @@
+// sidenav initialization
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // 
