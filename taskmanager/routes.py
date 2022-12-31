789d70de524ff585f93e3276e962873ab1fc967b
Alan Cruz da Silva <alancruzsilva@gmail.com>
2022-12-31 17:18:36 +0000

    Add task template and functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -33,3 +33,54 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+# Look into defensive programming and error handling
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    """
+    edit category function
+    """
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    """
+    retrieves the category id or throws 404 error if there isn't one
+    deletes the category from the database
+    commits the delete to the database
+    redirects the user to the categories page
+    """
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
+
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    """
+    When the user clicks add task button - form template is rendered.
+    When displaying the form we're using the GET method to get the page.
+    When the user submits the form it uses POST method
+    """
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task = Task(
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
+            is_urgent=bool(True if request.form.get("is_urgent") else False),
+            due_date=request.form.get("due_date"),
+            category_id=request.form.get("category_id")
+        )
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_task.html", categories=categories)
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,23 @@
-// sidenav initialization
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select category dropdown initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    // collapsible initialization 
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
